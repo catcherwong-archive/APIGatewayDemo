36193620c4236a5a1e0f0db7e8d06109bf82f65e
catcherwong <catcher_hwq@outlook.com>
2017-12-07 22:28:36 +0800

    :sparkles: Add Logging Demo.

---
+++ APIGatewayLoggingDemo/CustomerServices/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿namespace CustomerServices.Controllers
+{
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Mvc;
+    
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+            
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
