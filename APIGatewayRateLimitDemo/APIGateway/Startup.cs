e1b3ff3c48f9a0927f0709067055e0b5d135b91a
catcherwong <catcher_hwq@outlook.com>
2018-02-11 12:30:20 +0800

    :sparkles: Add RateLimit Demo.

---
+++ APIGatewayRateLimitDemo/APIGateway/Startup.cs
@@ -0,0 +1,41 @@
+﻿namespace APIGateway
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using Ocelot.DependencyInjection;
+    using Ocelot.Middleware;
+
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new Microsoft.Extensions.Configuration.ConfigurationBuilder();
+            builder.SetBasePath(env.ContentRootPath)
+                   .AddJsonFile("appsettings.json")
+                   //add configuration.json
+                   .AddJsonFile("configuration.json", optional: false, reloadOnChange: true)
+                   .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddOcelot(Configuration);
+        }
+
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            //console logging
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+
+            app.UseOcelot().Wait();
+        }
+    }
+}
