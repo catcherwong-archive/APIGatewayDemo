52cf4ce60977b786d46786cbe3f9273edba21e90
catcherwong <catcher_hwq@outlook.com>
2018-03-11 11:46:45 +0800

    :sparkles: Add QoS Demo.

---
+++ APIGatewayQoSDemo/APIServices/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿namespace APIServices.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
+    using System.Threading;
+
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            System.Console.WriteLine("get...");
+            Thread.Sleep(5000);
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
