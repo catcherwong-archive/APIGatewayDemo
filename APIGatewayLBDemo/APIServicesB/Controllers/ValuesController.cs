91272dd8c0f242d01dfe6517f435861d3859770c
catcherwong <catcher_hwq@outlook.com>
2018-03-13 20:22:51 +0800

    :sparkles: Add Load Balance Demo.

---
+++ APIGatewayLBDemo/APIServicesB/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIServicesB.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "From APIServiceB";
+        }        
+    }
+}
