cfcaa05ed8041c5a3efb74f7920a33680ff0e204
catcherwong <catcher_hwq@outlook.com>
2018-05-17 22:51:43 +0800

    :construction: Add Service Discovery Demo.

---
+++ APIGatewaySDDemo/APIGateway/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace APIGateway
+{
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Ocelot.DependencyInjection;
+    using Ocelot.Middleware;
+    
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+               .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config
+                        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)                      
+                        .AddJsonFile("ocelot.json")
+                        .AddEnvironmentVariables();
+                })
+               .ConfigureServices(s =>
+                {
+                    s.AddOcelot();
+                })
+                .Configure(a =>
+                {
+                    a.UseOcelot().Wait();
+                })
+                .Build();
+    }
+}
