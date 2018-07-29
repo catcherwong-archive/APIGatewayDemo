5a01459ca71b2c172476b339d3c19b0bf5d315b8
catcherwong <catcher_hwq@outlook.com>
2018-07-29 16:31:01 +0800

    :arrow_up: Update to .NET Core 2.1

--- APIGatewayLoggingDemo/APIGateway/Startup.cs
+++ APIGatewayLoggingDemo/APIGateway/Startup.cs
@@ -1,46 +1,46 @@
-﻿namespace APIGateway
-{    
-    using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Logging;
-    using NLog.Extensions.Logging;
-    using Ocelot.DependencyInjection;
-    using Ocelot.Middleware;
+﻿// namespace APIGateway
+// {    
+//     using Microsoft.AspNetCore.Builder;
+//     using Microsoft.AspNetCore.Hosting;
+//     using Microsoft.Extensions.Configuration;
+//     using Microsoft.Extensions.DependencyInjection;
+//     using Microsoft.Extensions.Logging;
+//     using NLog.Extensions.Logging;
+//     using Ocelot.DependencyInjection;
+//     using Ocelot.Middleware;
 
-    public class Startup
-    {
-        public Startup(IHostingEnvironment env)
-        {
-            var builder = new Microsoft.Extensions.Configuration.ConfigurationBuilder();
-            builder.SetBasePath(env.ContentRootPath)
-                   .AddJsonFile("appsettings.json")
-                   //add configuration.json
-                   .AddJsonFile("configuration.json", optional: false, reloadOnChange: true)
-                   .AddEnvironmentVariables();
+//     public class Startup
+//     {
+//         public Startup(IHostingEnvironment env)
+//         {
+//             var builder = new Microsoft.Extensions.Configuration.ConfigurationBuilder();
+//             builder.SetBasePath(env.ContentRootPath)
+//                    .AddJsonFile("appsettings.json")
+//                    //add configuration.json
+//                    .AddJsonFile("configuration.json", optional: false, reloadOnChange: true)
+//                    .AddEnvironmentVariables();
 
-            Configuration = builder.Build();
-        }
+//             Configuration = builder.Build();
+//         }
 
-        public IConfigurationRoot Configuration { get; }
+//         public IConfigurationRoot Configuration { get; }
         
-        public void ConfigureServices(IServiceCollection services)
-        {            
+//         public void ConfigureServices(IServiceCollection services)
+//         {            
 
-            services.AddOcelot(Configuration);
-        }
+//             services.AddOcelot(Configuration);
+//         }
 
-        public void Configure(IApplicationBuilder app ,ILoggerFactory loggerFactory)
-        {
-            //console logging
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+//         public void Configure(IApplicationBuilder app ,ILoggerFactory loggerFactory)
+//         {
+//             //console logging
+//             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
 
-            //nlog logging
-            loggerFactory.AddNLog();
-            loggerFactory.ConfigureNLog("nlog.config");
+//             //nlog logging
+//             loggerFactory.AddNLog();
+//             loggerFactory.ConfigureNLog("nlog.config");
 
-            app.UseOcelot().Wait();
-        }
-    }
-}
+//             app.UseOcelot().Wait();
+//         }
+//     }
+// }
